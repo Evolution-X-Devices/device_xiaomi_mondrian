bb8a3c9aab2b139a85e742a88886af820320dbf2
flakeforever <113103193+flakeforever@users.noreply.github.com>
2025-07-13 11:48:48 -0700

    mondrian: Switch camera config to select

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,8 @@ TARGET_BOOTLOADER_BOARD_NAME := taro
 TARGET_NO_BOOTLOADER := true
 
 # Camera
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-TARGET_CAMERA_PACKAGE_NAME := com.android.camera
+$(call soong_config_set, camera, override_format_from_reserved, true)
+$(call soong_config_set, camera,package_name, com.android.camera)
 
 # DTB
 # TARGET_PREBUILT_DTB := $(KERNEL_PATH)/dtb
